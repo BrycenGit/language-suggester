b2aa19ca31c6d420cebbb4c97a5f1f5d21b9d8d8
[brycen] <[brycenbartolome@gmail.com]>
2020-08-14 16:31:37 -0700

    final clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-
-  
-
 $(document).ready(function() {
   $("#nameForm").submit(function(event) {
     event.preventDefault();
@@ -31,15 +28,8 @@ $(document).ready(function() {
     const answer3 = parseInt($("input:radio[name=q3]:checked").val());
     const answer4 = parseInt($("input:radio[name=q4]:checked").val());
     const answer5 = parseInt($("input:radio[name=q5]:checked").val());
-    console.log(answer1);
-    console.log(answer2);
-    console.log(answer3);
-    console.log(answer4);
-    console.log(answer5);
     const addTotal = (answer1) + (answer2) + (answer3) + (answer4) + (answer5); 
-    console.log(addTotal);
     const result = (Math.round(addTotal/5));
-    console.log(result);
 
     if (result > 3) {
       $("#ruby").show();
